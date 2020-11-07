a8b8f2b1a69b92d7ee58af98a69933e6b5c95d94
akira kashima <a.kashima@icloud.com>
2020-11-07 20:57:09 +0900

    verify test

--- roadworker_installer.sh
+++ roadworker_installer.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+
+# rvenv installing
 echo it installing rbenv now...
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
@@ -10,12 +12,14 @@ cd ~/.rbenv/plugins
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 source ~/.zshrc
 
+# rvenv specify version
 echo it installing ruby now...
 rbenv install -l
 rbenv install 2.4.3
 rbenv global 2.4.3
 eval "$(rbenv init -)"
 
+# roadworker install
 echo roadworker use middle ware...
 sudo yum install git make gcc libxml2-devel libxslt-devel libpcap-devel ruby-devel
 
